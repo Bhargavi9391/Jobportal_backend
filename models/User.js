06b1dcfe491909c0ef847060bd38c7de316b6a37
jaddu Bhargavi <157037629+Bhargavi9391@users.noreply.github.com>
2025-11-15 20:27:42 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false },
-  date: { type: Date, default: Date.now }
+  isAdmin: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('User', userSchema);
